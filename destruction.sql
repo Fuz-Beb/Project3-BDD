0db6ed1d961f42554f4960782d0c022ac3122589
Pierrick Bobet - Bebo <pierrick.bobet@cryptolab.net>
2017-10-15 15:49:32 -0400

    ajout fichier conformité

---
+++ destruction.sql
@@ -0,0 +1,19 @@
+﻿-- Suppression de l'ensemble des tables de la base de donnée --
+
+-- Suppression de la table Avocat
+DROP TABLE IF EXISTS "Avocat" CASCADE;
+
+-- Suppression de la table Juge
+DROP TABLE IF EXISTS "Juge" CASCADE;
+
+-- Suppression de la table Jury
+DROP TABLE IF EXISTS "Jury" CASCADE;
+
+-- Suppression de la table Partie
+DROP TABLE IF EXISTS "Partie" CASCADE;
+
+-- Suppression de la table Proces
+DROP TABLE IF EXISTS "Proces" CASCADE;
+
+-- Suppression de la table Seance
+DROP TABLE IF EXISTS "Seance" CASCADE;
