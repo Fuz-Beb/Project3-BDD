c16c7a4d4115f2376681f0319b1b6aa699004714
Pierrick Bobet - Bebo <pierrick.bobet@cryptolab.net>
2017-11-20 17:57:56 -0500

    assignerProces: commit manquant + validation manquante

--- src/tp3/GestionJury.java
+++ src/tp3/GestionJury.java
@@ -94,8 +94,8 @@ public class GestionJury
                 throw new IFT287Exception("Proces n'existe pas : " + tupleProces.getId());
             if (!proces.devantJury(tupleProces))
                 throw new IFT287Exception("Le proces " + tupleProces.getId() + " doit se tenir devant un juge seul");
-            if (tupleJury.getProces_id() == -1)
-                throw new IFT287Exception("Le jury " + tupleProces.getId() + " a déjà un procès");
+            if (!(tupleJury.getProces_id() == -1))
+                throw new IFT287Exception("Le jury " + tupleJury.getNas() + " a déjà un procès");
             jury.assignerProces(tupleJury, tupleProces);
             
             cx.commit();
